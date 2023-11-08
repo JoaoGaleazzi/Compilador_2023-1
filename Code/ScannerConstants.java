e8330eb5414d6ca4bf35e4564a1945be2fab254c
João Henrique Stolf Galeazzi <jgaleazzi@furb.br>
2023-11-08 20:59:07 -0300

    gerado o código do novo gals

--- Code/ScannerConstants.java
+++ Code/ScannerConstants.java
@@ -1060,12 +1060,10 @@ public interface ScannerConstants
 
     String[] SCANNER_ERROR =
     {
-        //"Caractere n�o esperado",v
-        "símbolo não reconhecido",
+        "Caractere n�o esperado",
         "",
         "",
-        //"Erro identificando constante_string",v
-        "",
+        "Erro identificando constante_string",
         "",
         "",
         "",
@@ -1083,28 +1081,24 @@ public interface ScannerConstants
         "",
         "",
         "Erro identificando <ignorar>",
-        //"Erro identificando identificador",v
-        "sequência não reconhecida",
+        "Erro identificando identificador",
         "",
         "",
         "",
         "",
         "",
         "",
-        //"Erro identificando constante_string",v
-        "",
+        "Erro identificando constante_string",
         "Erro identificando constante_float",
         "",
         "",
-        //"Erro identificando identificador",v
-        "sequência não reconhecida",
+        "Erro identificando identificador",
         "",
         "",
         "",
         "Erro identificando constante_float",
         "",
-        //"Erro identificando identificador",v
-        "sequência não reconhecida",
+        "Erro identificando identificador",
         ""
     };
 
